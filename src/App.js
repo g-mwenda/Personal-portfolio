8e9bb823674e7f60e2a9d58b59f7c27fba514b80
Gabriel Mwenda <gabrielmwenda0001@gmail.com>
2023-09-21 15:11:26 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { NavBar } from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
